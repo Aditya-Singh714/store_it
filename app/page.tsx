dcd72c88b23265ce54c21084d533fbfd3509636c
Aditya-Singh714 <adityajaat0000@gmail.com>
2024-11-08 22:24:50 +0530

    Design System, Theming and Tailwind config

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-   <div>
-      <h1 className="text-3xl text-green-900">
+   <div className="flex-center h-screen">
+      <h1 className="text-3xl text-brand-100">
         StoreIt - The only storage solution you need
       </h1>
    </div>
