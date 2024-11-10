56837ba9b1c4e9b67de66cd86748161d61de1e67
Aditya-Singh714 <adityajaat0000@gmail.com>
2024-11-10 13:50:07 +0530

    Setup Appwrite & Implemented Create Account

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -17,6 +17,7 @@ import { Input } from "@/components/ui/input";
 import Image from "next/image";
 import { useState } from "react";
 import Link from "next/link";
+import { createAccount } from "@/lib/actions/user.actions";
 
 const formSchema = z.object({
   username: z.string().min(2).max(50),
@@ -37,6 +38,7 @@ const authFormSchema = (formType: FormType) => {
 const AuthForm = ({ type }: { type: FormType }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const [accountId, setAccountId] = useState(null);
 
   const formSchema = authFormSchema(type);
   const form = useForm<z.infer<typeof formSchema>>({
@@ -46,9 +48,25 @@ const AuthForm = ({ type }: { type: FormType }) => {
       email: "",
     },
   });
+
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    setIsLoading(true);
+    setErrorMessage("");
+    
+    try {
+      const user = await createAccount({
+        fullName: values.fullName || "",
+        email: values.email,
+      });
+
+      setAccountId(user.accountId);
+    } catch {
+      setErrorMessage("Failed to create account. Please try again");
+    } finally {
+      setIsLoading(false);
+    }
   };
+
   return (
     <>
       <Form {...form}>
