4fbc3010428224ed833942dddb3ce848b4cb8c52
Aditya-Singh714 <adityajaat0000@gmail.com>
2024-11-13 13:57:21 +0530

    Implemented Upload Files functionality

--- components/MobileNavigation.tsx
+++ components/MobileNavigation.tsx
@@ -1,6 +1,5 @@
-"use client";
 
-import React, { useState } from "react";
+"use client";
 
 import {
   Sheet,
@@ -9,17 +8,18 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import Image from "next/image";
-import { Separator } from "@/components/ui/separator";
+import React, { useState } from "react";
 import { usePathname } from "next/navigation";
+import { Separator } from "@radix-ui/react-separator";
 import { navItems } from "@/constants";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
-import FileUploader from "./FileUploader";
-import { Button } from "./ui/button";
+import { Button } from "@/components/ui/button";
+import FileUploader from "@/components/FileUploader";
 import { signOutUser } from "@/lib/actions/user.actions";
 
 interface Props {
-  ownerid: string;
+  $id: string;
   accountId: string;
   fullName: string;
   avatar: string;
@@ -27,7 +27,7 @@ interface Props {
 }
 
 const MobileNavigation = ({
-  ownerId,
+  $id: ownerId,
   accountId,
   fullName,
   avatar,
@@ -43,17 +43,28 @@ const MobileNavigation = ({
         alt="logo"
         width={120}
         height={52}
+        className="h-auto"
       />
 
       <Sheet open={open} onOpenChange={setOpen}>
         <SheetTrigger>
-          <Image src="/icons/menu.svg" alt="Search" width={30} height={30} />
+          <Image
+            src="/icons/menu.svg"
+            alt="Search"
+            width={30}
+            height={30}
+          />
         </SheetTrigger>
         <SheetContent className="shad-sheet h-screen px-3">
           <SheetTitle>
             <div className="header-user">
-              <Image src={avatar} alt="avatar" width={44} height={44} />
-
+              <Image
+                src={avatar}
+                alt="avatar"
+                width={44}
+                height={44}
+                className="header-user-avatar"
+              />
               <div className="sm:hidden lg:block">
                 <p className="subtitle-2 capitalize">{fullName}</p>
                 <p className="caption">{email}</p>
@@ -69,7 +80,7 @@ const MobileNavigation = ({
                   <li
                     className={cn(
                       "mobile-nav-item",
-                      pathname === url && "shad-active"
+                      pathname === url && "shad-active",
                     )}
                   >
                     <Image
@@ -79,7 +90,7 @@ const MobileNavigation = ({
                       height={24}
                       className={cn(
                         "nav-icon",
-                        pathname === url && "nav-icon-active"
+                        pathname === url && "nav-icon-active",
                       )}
                     />
                     <p>{name}</p>
@@ -92,11 +103,11 @@ const MobileNavigation = ({
           <Separator className="my-5 bg-light-200/20" />
 
           <div className="flex flex-col justify-between gap-5 pb-5">
-            <FileUploader />
+            <FileUploader ownerId={ownerId} accountId={accountId} />
             <Button
               type="submit"
               className="mobile-sign-out-button"
-              onClick={async() => await signOutUser()}
+              onClick={async () => await signOutUser()}
             >
               <Image
                 src="/icons/logout.svg"
