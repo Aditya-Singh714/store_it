6f8e1b45d28c9c48395aa9da6804f0fb13b33c79
Aditya-Singh714 <adityajaat0000@gmail.com>
2024-11-19 13:18:11 +0530

    ignore build error

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 import Link from "next/link";
 import { Models } from "node-appwrite";
 
-import ActionDropdown from "@/components/ActionDropdown";
 import { Chart } from "@/components/Chart";
 import { FormattedDateTime } from "@/components/FormattedDateTime";
 import { Thumbnail } from "@/components/Thumbnail";
 import { Separator } from "@/components/ui/separator";
 import { getFiles, getTotalSpaceUsed } from "@/lib/actions/file.actions";
 import { convertFileSize, getUsageSummary } from "@/lib/utils";
+import ActionDropdown from "@/components/ActionDropdown";
 
 const Dashboard = async () => {
   // Parallel requests
